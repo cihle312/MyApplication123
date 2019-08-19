6d2a2ce39cc9c5736c68981ec2174073da99ca49
Sihle <sihle@debmower_adj>
2019-08-20 00:39:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication123/HomeFragment.java
@@ -0,0 +1,35 @@
+package com.example.myapplication123;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class HomeFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, null);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(getActivity(),"you are inside Home Fragment",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+}
